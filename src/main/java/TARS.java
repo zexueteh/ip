c02dea2f79fc4f257c30ffb2608fce097d17eec2
zexueteh <66759893+zexueteh@users.noreply.github.com>
2025-01-27 09:50:20 +0800

    feature: Introduce TARS Chatbot skeleton with hello & goodbye messages.

---
+++ src/main/java/TARS.java
@@ -0,0 +1,25 @@
+public class TARS {
+    public static void main(String[] args) {
+        printHelloMessage();
+        printGoodbyeMessage();
+    }
+
+    private static void printHelloMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("  _____  _    ____  ____  ");
+        System.out.println(" |_   _|/ \\  |  _ \\/ ___| ");
+        System.out.println("   | | / _ \\ | |_) \\___ \\ ");
+        System.out.println("   | |/ ___ \\|  _ < ___) |");
+        System.out.println("   |_/_/   \\_\\_| \\_\\____/ ");
+        System.out.println("                          ");
+        System.out.println("Hello, I'm TARS, your friendly chatbot assistant. ");
+        System.out.println("How can I help you today?");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printGoodbyeMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye and Goodnight! ");
+        System.out.println("____________________________________________________________");
+    }
+}
