3dbc8e7c9ef159a28a50a911fecc486a1e8e9a4f
zexueteh <66759893+zexueteh@users.noreply.github.com>
2025-02-17 00:32:57 +0800

    refactor: Implemented Level-5 error validation - Implemented parseCommandBody to validate command arguments and enforce proper structure. - Added validation for /by, /from, /to flags in DEADLINE and EVENT commands. - Ensured MARK and UNMARK commands validate task index before processing. - Refactored commandHandler() to improve separation of responsibility.

--- src/main/java/TARS.java
+++ src/main/java/TARS.java
@@ -50,33 +50,16 @@ public class TARS {
                 }
 
                 String commandBody = parseCommandBody(line, commandType);
+                commandHandler(commandType, commandBody);
+
 
             } catch (Exception e) {
                 printResponseMessage(e.getMessage());
+                System.out.println("ERROR");
             }
 
             // to refactor into command handler function once input validation impelemented
-//            switch (commandType) {
-//            case LIST:
-//                printTaskList();
-//                break;
-//            case MARK:
-//            case UNMARK:
-//                int index = parseMarkCommand(line);
-//                boolean isMarking = line.startsWith("mark");
-//                markHandler(index, isMarking);
-//                break;
-//
-//            case TODO:
-//            case EVENT:
-//            case DEADLINE:
-//                Task newTask = taskParser(line, commandType);
-//                if (newTask != null) {
-//                    addTask(newTask);
-//                }
-//                break;
-//            default:
-//                break;
+
 
             }
 
@@ -128,17 +111,16 @@ public class TARS {
     }
 
     private static String parseCommandBody(String line, CommandType commandType)  throws TARSInvalidCommandBodyException {
-        int lineLength = line.length();
+        String commandBody;
         line = line.trim();
 
         int commandStartIndex = line.indexOf(' ');
         if (commandStartIndex == -1) {
-            commandStartIndex = line.length();
+            commandStartIndex = line.length(); // single word command e.g. LIST
         } else {
             commandStartIndex = commandStartIndex + 1;
         }
-
-        String commandBody = line.substring(commandStartIndex, lineLength);
+        commandBody = line.substring(commandStartIndex);
 
 
         switch (commandType) {
@@ -147,6 +129,7 @@ public class TARS {
                 throw new TARSInvalidCommandBodyException();
             }
             break;
+
         case MARK:
         case UNMARK:
             int taskNumber;
@@ -155,13 +138,65 @@ public class TARS {
             } catch (NumberFormatException e) {
                 throw new TARSInvalidCommandBodyException();
             }
-            if (taskNumber > nTasks) {
+            if (taskNumber > nTasks || taskNumber < 1) {
                 throw new TARSInvalidCommandBodyException();
             }
+            break;
+
         case TODO:
-        case EVENT:
+            if (commandBody.isEmpty()
+                || commandBody.contains("/by")
+                || commandBody.contains("/from")
+                || commandBody.contains("/to")
+            ) {
+                throw new TARSInvalidCommandBodyException();
+            }
+            break;
+
         case DEADLINE:
+            if (commandBody.contains("/from")
+            || commandBody.contains("/to")
+            ) {
+                throw new TARSInvalidCommandBodyException();
+            }
+
+            if (!commandBody.contains("/by")) {
+                throw new TARSInvalidCommandBodyException();
+            }
+
+            int byIndex = commandBody.indexOf("/by");
+            String byString = commandBody.substring(byIndex + 3);
+            if (byString.isEmpty()) {
+                throw new TARSInvalidCommandBodyException();
+            }
+            break;
+
+        case EVENT:
             // To be implemented
+            if (commandBody.contains("/by")) {
+                throw new TARSInvalidCommandBodyException();
+            }
+
+            if (!commandBody.contains("/from")
+                || !commandBody.contains("/to")
+            ) {
+                throw new TARSInvalidCommandBodyException();
+            }
+
+            int fromIndex = commandBody.indexOf("/from");
+            int toIndex = commandBody.indexOf("/to");
+
+            if (fromIndex > toIndex) {
+                throw new TARSInvalidCommandBodyException();
+            }
+
+            String fromString = commandBody.substring(fromIndex + 5, toIndex);
+            String toString = commandBody.substring(toIndex + 3);
+
+            if (fromString.isEmpty() || toString.isEmpty()) {
+                throw new TARSInvalidCommandBodyException();
+            }
+
             break;
         default:
             break;
@@ -215,9 +250,10 @@ public class TARS {
     /**
      * Marks/Unmarks task in taskList
      * @param index index of Task in taskList
-     * @param isMarking differentiates mark/unmark
+     * @param commandType enum CommandType MARK, UNMARK
      */
-    private static void markHandler(int index, boolean isMarking) {
+    private static void markHandler(int index, CommandType commandType) {
+        boolean isMarking = commandType == CommandType.MARK;
         taskList[index].setIsDone(isMarking);
 
         System.out.println(LINE_SEPERATOR);
@@ -234,41 +270,35 @@ public class TARS {
      * takes user input line and parses out Task attributes to return Task Object
      * parsing logic depends on command type
      * WARNING: only basic input validation implemented
-     * @param line user input String as a command
-     * @param commandType first word in line representing type of command
+     * @param commandBody user input string
+     * @param commandType enum CommandType TODO, DEADLINE, EVENT
      * @return Todo, Deadline or Event Object
      */
-    private static Task taskParser(String line, CommandType commandType) {
+    private static Task getTask(String commandBody, CommandType commandType) {
         Task newTask = null;
-        String description = null;
+        String description = commandBody;
         String by  = null;
         String from = null;
         String to = null;
 
         // Eg. event E /from a /to b
-        //     commandType = "event";  commandBody = "E /from a /to b";
-        String commandBody = line.substring(line.indexOf(" ") + 1);
+        //     commandType = CommandType.EVENT;  commandBody = "E /from a /to b";
 
-        // Parsing the line String
-        description = commandBody;
-
-        // Handle Deadline command containing /by
-        int byIndex = commandBody.indexOf("/by");
-        if (byIndex != -1) {
+        switch (commandType) {
+        case DEADLINE: // Handle Deadline command containing /by
+            int byIndex = commandBody.indexOf("/by");
             by = commandBody.substring(byIndex + 3).trim();
             description = commandBody.substring(0, byIndex).trim();
-        }
+            break;
 
-        // Handle Event command containing /from /to
-        int fromIndex = commandBody.indexOf("/from");
-        int toIndex = commandBody.indexOf("/to");
+        case EVENT: // Handle Event command containing /from /to
+            int fromIndex = commandBody.indexOf("/from");
+            int toIndex = commandBody.indexOf("/to");
 
-        if (fromIndex != -1 && toIndex != -1 && fromIndex < toIndex) {
             from = description.substring(fromIndex + 5, toIndex).trim();
             to = description.substring(toIndex + 3).trim();
             description = description.substring(0, fromIndex).trim();
         }
-
 
         // Return Task object based on commandType
         switch (commandType) {
@@ -279,10 +309,35 @@ public class TARS {
             newTask = new Deadline(description, by);
             break;
         case EVENT:
-            newTask = new Event(description, to, from);
+            newTask = new Event(description, from, to);
             break;
         }
         return newTask;
     }
 
+    private static void commandHandler (CommandType commandType, String commandBody) {
+        switch (commandType) {
+        case LIST:
+            printTaskList();
+            break;
+
+        case MARK:
+        case UNMARK:
+            int index = parseMarkCommand(commandBody);
+            markHandler(index, commandType);
+            break;
+
+        case TODO:
+        case EVENT:
+        case DEADLINE:
+            Task newTask = getTask(commandBody, commandType);
+            addTask(newTask);
+            break;
+
+        default:
+            break;
+
+        }
+    }
 }
+
